1a085a789a1e8ae3f05a4f3c71c3337940f005a5
David Hall <dlwh@stanford.edu>
2022-04-14 13:50:58 -0700

    try better split

--- examples/split_pile.py
+++ examples/split_pile.py
@@ -1,6 +1,9 @@
 import json
+import os
+
 import fsspec
 import zstandard
+import re
 
 _HOST_URL = "https://mystic.the-eye.eu"
 
@@ -45,6 +48,13 @@ def create_output_path_for_set(in_url, split_name, pile_set):
     else:
         return f"data/{pile_set}/{split_name}.jsonl.zst"
 
+# python is very slow, so we're going to avoid parsing the json, and instead just extract the .meta.pile_set_name
+# without parsing the json.
+# The json looks like {"text": "blah blah blah", "meta": {"pile_set_name": "Pile-CC"}}
+extract_re = re.compile(r'"meta": {"pile_set_name": "(?P<pile_set_name>.*)"}}\n?$')
+def get_split_name(line):
+    return extract_re.search(line).group("pile_set_name")
+
 def download_and_split(split_name, url):
     """
     Download the pile from the url and split it into the split_name.
@@ -55,14 +65,18 @@ def download_and_split(split_name, url):
     try:
         with fsspec.open(url, mode='r', compression="infer") as f:
             for line in f:
-                doc = json.loads(line)
-                subset = doc["meta"]["pile_set_name"]
+                # doc = json.loads(line)
+                # subset = doc["meta"]["pile_set_name"]
+                subset = get_split_name(line)
                 subset = pile_sets[subset]
                 out_files[subset].write(line.encode("utf-8"))
     finally:
         for subset, out_file in out_files.items():
             out_file.close()
 
+# create dirs
+for pile_set in pile_sets.values():
+    os.makedirs(f"data/{pile_set}", exist_ok=True)
 
 for split, urls in splits.items():
     for i, url in enumerate(urls):
