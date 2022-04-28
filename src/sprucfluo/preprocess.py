59276bb5edaec5990faf7bdf69154491ed5703cb
David Hall <dlwh@stanford.edu>
2022-04-27 22:46:01 -0700

    switch to using parquet for file types, about ~50% savings in storage
    also less dependency on HF, which is always nice.

--- src/sprucfluo/preprocess.py
+++ src/sprucfluo/preprocess.py
@@ -1,57 +1,53 @@
+##########
+# THIS CODE IS STILL EXPERIMENTAL AND YOU SHOULDN'T USE IT YET.
+########
+
 # IterDataPipe for preprocessing data, tokenizing, and caching to disk
-# The general file format we're going with is an apache arrow file with (for now) just one column
-# for the tokens. A row is a single doc.
-# Once we have this one column, we can slide a window of size n over the data and feed it directly to dataloaders
-# TODO: explore maybe using Parquet
-# (We might add back in file metadata later, or add support for other columns)
+# The general file format we're going with is an apache parquet file with columns for the output of the tokenizer,
+# A row is a single doc
+# (We might add back in file metadata later)
 # We don't want to have one giant file, so we'll split it up into chunks.
-# TODO: want to also handle being interrupted mid-file, and continuing where we left off.
-# TODO: also want to support streaming
 import functools
 import json
+import multiprocessing as mp
 import os
-from typing import Iterator, Iterable, Any, Optional, Sequence, Dict, List
+from typing import Iterator, Optional
 
 import numpy as np
 import pyarrow as pa
+import pyarrow.parquet as pq
+from torch.utils.data import IterDataPipe
 from torchdata.datapipes.iter import IterableWrapper
-from datasets.arrow_writer import ArrowWriter
-from datasets.arrow_reader import ArrowReader
-from torch.utils.data import DataLoader, IterableDataset, IterDataPipe
 from tqdm import tqdm
 from transformers import BatchEncoding, AutoTokenizer, PreTrainedTokenizerFast
 
-import multiprocessing as mp
-
-
-# As a heuristic, we're aiming for files that are around 200MB, which is around 25 million tokens for 32-bit (TODO: 16-bit)
+# As a heuristic, we're aiming for files that are around ~250MB
 # Typically we're training on sequences of length ~1024 and batch size up to 512, so better to make it divisible by that.
-# 512 * 1024 = 512Ki, so we'll go with 128 * 512 * 1024 = 67108864 tokens, which is about 100MB
-from sprucfluo import concatenate_and_group_texts
-from sprucfluo.text import batch_tokenize
+# 4bytes * 512 * 1024 = 2Mi, so we'll go with 128 * 512 * 1024 = 67108864 tokens, which is about 256MiB
 
 NUM_TOKENS_PER_FILE = 67108864
 
-# TODO: implement a dataset that forks a process that eagerly loops over files filling up the cache,
-# while the main dataset is just pulling from the cache. Have to be clever about semaphores
-# when it reads from the cache, it just grabs the "chunks" from the chunked array and splits
-# them into appropriate sizes for batching. Can just be a transpose
-# TODO: make the dataset capable of just reading from disk if the cache is complete
-# TODO: maybe implement seeking, so we can resume where we left off if there's a crash
-# probably easiest to just mark a cache complete when you finish a chunk
+
+# TASKS:
+# TODO: only do the caching on local_rank=0 so we do it once per device
+# TODO: figure out how to best do multiple nodes
+# TODO: figure out resuming preprocessing jobs
+# TODO: make sure we handle reentrancy correctly in the dataset
+# TODO: some kind of checksumming for ledger
+# TODO: want to also handle being interrupted mid-file, and continuing where we left off.
 
 LEDGER_FILE = "ledger.json"
 
 def cache_encodings(
         doc_iter: Iterator[BatchEncoding],
         out_dir: str,
-        file_template: str = 'docs-{}.arrow',
+        file_template: str = 'docs-{}.parquet',
         num_tokens_per_file: int = NUM_TOKENS_PER_FILE) -> Iterator:
     """
     Given an iterator of documents, groups and caches them to disk, and yield them in batches of NUM_TOKENS_PER_FILE
     """
     file_index = 0
-    current_arrow_writer: Optional[ArrowWriter] = None
+    current_writer: Optional[pq.ParquetWriter] = None
     current_num_tokens = 0
     tq: Optional[tqdm] = tqdm(desc=f"file {file_index} progress", total=num_tokens_per_file, unit="token")
     file_out: Optional[str] = None
@@ -59,38 +55,44 @@ def cache_encodings(
     # list of (file_name, num_tokens), to be output at the end if we finish the whole iterator
     ledger = []
 
-    def reset_writer():
-        nonlocal current_arrow_writer, tq, file_out
+    def reset_writer(schema):
+        nonlocal current_writer, tq, file_out
         file_out = f"{out_dir}/{file_template.format(file_index)}"
         os.makedirs(os.path.dirname(file_out), exist_ok=True)
-        current_arrow_writer = ArrowWriter(path=file_out)
+        current_writer = pq.ParquetWriter(file_out, schema, version="2.6", compression="ZSTD")
 
         tq.reset()
         tq.set_description(f"file {file_index}")
 
-    reset_writer()
+    def as_record_batch(doc):
+        names, columns = zip(*[(k, pa.array(v)) for k, v in doc.items() ])
+        return pa.RecordBatch.from_arrays(list(columns), names)
 
     try:
         for doc in doc_iter:
+            batch = as_record_batch(doc)
             batch_len = sum(len(t) for t in doc["input_ids"])
-            # TODO: need to adjust logic for if batch_len > num_tokens_per_file
-            if current_num_tokens + batch_len > num_tokens_per_file:
-                current_arrow_writer.close()
-                current_arrow_writer = None
+            if not current_writer:
+                reset_writer(batch.schema)
+            # NB: the elif means we'll write to this file if it's brand new even if the batch is too big
+            # TODO: should we maybe split the batch if it's too big?
+            elif current_num_tokens + batch_len > num_tokens_per_file:
+                current_writer.close()
+                current_writer = None
                 ledger.append({"file_name": file_out, "num_tokens": current_num_tokens})
                 yield file_out
                 file_index += 1
-                reset_writer()
-
                 current_num_tokens = 0
-            current_arrow_writer.write_batch(doc)
+                reset_writer(batch.schema)
+
+            current_writer.write_batch(batch)
             current_num_tokens += batch_len
             tq.update(batch_len)
 
-        if current_arrow_writer:
+        if current_writer:
             tq.reset(current_num_tokens)
             tq.update(current_num_tokens)
-            current_arrow_writer.close()
+            current_writer.close()
             if current_num_tokens > 0:
                 ledger.append({"file_name": file_out, "num_tokens": current_num_tokens})
         # if we successfully wrote the whole iterator, we can write the ledger
@@ -101,8 +103,8 @@ def cache_encodings(
             json.dump(ledger, f)
         yield file_out
     except KeyboardInterrupt:
-        current_arrow_writer.close()
-        current_arrow_writer = None
+        current_writer.close()
+        current_writer = None
         if file_out:
             os.remove(file_out)
         raise
@@ -113,7 +115,8 @@ def read_cache(file, flatten: bool=False) -> Iterator[BatchEncoding]:
     If flatten is false, this returns the docs as they were presented to the caching process. If flatten is True,
     then the documents returned are actually concatenated documents, where the number is the number of documents
     presented as a batch to the caching process."""
-    for b in ArrowReader.read_table(file).to_batches():
+    # for b in ArrowReader.read_table(file).to_batches():
+    for b in pq.read_table(file).to_batches():
         if flatten:
             # insert a newaxis to the beginning so that it appears to be bs=1
             yield BatchEncoding(
@@ -132,7 +135,7 @@ class EagerCachingDataset(IterDataPipe[BatchEncoding]):
                  doc_iter: IterDataPipe[BatchEncoding],
                  cache_dir: str,
                  num_tokens_per_file: int = NUM_TOKENS_PER_FILE,
-                 file_template: str = 'docs-{}.arrow'):
+                 file_template: str = 'docs-{}.parquet'):
         self.doc_iter = doc_iter
         self.cache_dir = cache_dir
         self.num_tokens_per_file = num_tokens_per_file
@@ -184,7 +187,7 @@ if __name__ == '__main__':
         .readlines(return_path=False) \
         .map(json.loads) \
         .map(lambda x: x["text"]) \
-        .then(batch_tokenize, tokenizer=tokenizer)
+        .then(batch_tokenize, tokenizer=tokenizer, return_attention_mask=False)
     ds = EagerCachingDataset(pipeline, "cache").flatmap(functools.partial(concatenate_and_group_texts, seq_len=512))
     total_tokens = 0
     for i, batch in enumerate(ds):
