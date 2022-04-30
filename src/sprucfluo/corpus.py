10028c11c8568b6036d421d1ad86325e99f209fc
David Hall <dlwh@stanford.edu>
2022-04-29 22:31:10 -0700

    switch back to 'r' mode for opening files for now...

--- src/sprucfluo/corpus.py
+++ src/sprucfluo/corpus.py
@@ -46,7 +46,7 @@ def load_corpus(paths: Union[str, List[str]],
         paths = paths.shard_by_rank()
 
     return paths \
-        .open_file_by_fsspec_fancy(expand_globs=expand_globs, mode="rb", compression="infer", **extra_fsspec_args) \
+        .open_file_by_fsspec_fancy(expand_globs=expand_globs, mode="r", compression="infer", **extra_fsspec_args) \
         .flatmap(lambda name_stream: read_lm_text_file(name_stream[0], name_stream[1], json_text_key))
 
 
