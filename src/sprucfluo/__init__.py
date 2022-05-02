50504fde18378f73966fa47ad1a813cc890b69e1
David Hall <dlwh@stanford.edu>
2022-05-02 11:45:15 -0700

    add slicing to iterdatapipes

--- src/sprucfluo/__init__.py
+++ src/sprucfluo/__init__.py
@@ -20,6 +20,8 @@ from .sharding import ShardByRankDataPipe
 from .text import concatenate_and_group_texts, tokenize_and_group_texts, read_lm_text_file
 from .corpus import load_corpus
 from .shuffle import SeededShufflerIterDataPipe
+from .slicing import SliceIterDataPipe
+
 
 _T = TypeVar("_T", contravariant=True)
 _U = TypeVar("_U", covariant=True)
@@ -34,12 +36,11 @@ def _then_data_pipe(data_pipe: IterDataPipe[_T], fn, *args, **kwargs) -> IterDat
 
 
 def init():
-    IterDataPipe.register_function("then", _then_data_pipe)
     """
     Initialize the sprucfluo package, which registers a bunch of methods and such with IterDataPipe.
     You don't need to call this function, because it's called automatically when you import sprucfluo.
     """
-    pass
+    IterDataPipe.register_function("then", _then_data_pipe)
 
 
 __all__ = [
@@ -50,6 +51,7 @@ __all__ = [
     'ShardByRankDataPipe',
     'expand_paths',
     'SeededShufflerIterDataPipe',
+    'SliceIterDataPipe',
 ]
 
 init()
