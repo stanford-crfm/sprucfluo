c84c46ad4d48efee08284d82aa77682deef3b8a6
David Hall <dlwh@stanford.edu>
2022-05-03 09:59:12 -0700

    remove cycle option in the presence of new sharding

--- src/sprucfluo/corpus.py
+++ src/sprucfluo/corpus.py
@@ -20,9 +20,9 @@ from torchdata.datapipes.iter import IterableWrapper
 from .files import expand_paths
 from .text import read_lm_text_file
 
+
 def load_corpus(paths: Union[str, List[str]],
                 shard_by_rank: bool = True,
-                cycle: bool = False,
                 json_text_key: str = "text",
                 extra_fsspec_args: Optional[Dict[str, Any]] = None,
                 expand_globs: bool = False) -> IterDataPipe[str]:
@@ -32,19 +32,14 @@ def load_corpus(paths: Union[str, List[str]],
     Args:
         paths: A list of paths to the corpus. Will be expanded via braceexpand.
         shard_by_rank: If True, each shard will be assigned to a different rank, as per pytorch RANK
-        cycle: If True, the corpus will be cycled to produce an infinite stream.
         json_text_key: The key in the JSON file to use as the text. Defaults to "text".
         extra_fsspec_args: Extra arguments to pass to fsspec. This can be used for authentication, etc.
         expand_globs: If True, will expand globs in the paths. This happens after the paths are expanded via braceexpand.
     """
     if extra_fsspec_args is None:
         extra_fsspec_args = {}
+
     paths = expand_paths(paths)
-
-    if cycle:
-        paths = paths.cycle()
-
-    extra_fsspec_args = extra_fsspec_args or {}
 
     if shard_by_rank:
         return paths.flat_shard_by_rank(
